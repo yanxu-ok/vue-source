a8776a739830714c30616369a09ce60f07469355
闫旭 <2250317861@qq.com>
2021-04-22 11:47:58 +0800

    模板编译完成

--- readme.md
+++ readme.md
@@ -4,11 +4,14 @@
  * @Author: 闫旭
  * @Date: 2021-04-20 17:49:07
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-04-21 18:12:13
+ * @LastEditTime: 2021-04-22 11:47:39
 -->
 ## 1.vue 监控对象的变化
 ## 2.vue 监控数组的变化，改写方法，并且如果是数组，需要对其中的对象进行遍历  并且新增方法如果是对象也需要监控。
-## 3.处理render方法  先编译html字符串得到需要的数据
+## 3.处理render方法 
+  1. 模板编译  先编译html字符串得到需要的数据
+  2. 构建ast数
 ### vue 知识点
 1. Object.created() 创建一个对象,对象的__proto__为括号当中的参数
 2. Array.prototype['push'].call(this,...arg)  这个this就是调用方法的当前数组,
+3. match 正则表达式方法 分组 (?:) 表示 这个分组用match匹配但是捕获，就是打印的话不显示
--- src/compiler/index.js
+++ src/compiler/index.js
@@ -4,7 +4,7 @@
  * @Author: 闫旭
  * @Date: 2021-04-21 10:09:22
  * @LastEditors: sueRimn
- * @LastEditTime: 2021-04-21 16:25:53
+ * @LastEditTime: 2021-04-22 11:40:10
  */
 // 这里正则比较恶心
 // 字符串里的 反斜杠
@@ -13,24 +13,97 @@ const qnameCapture = `((?:${cname}\\:)?${cname})`; // 获取标签名
 const startTagOpen = new RegExp(`^<${qnameCapture}`); // 获取开始标签
 const endTag = new RegExp(`^</${qnameCapture}[^>]*>`); // <xxx    > 获取关闭标签
 // 获取属性和值的
-const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"[^"]*")|(?:'[^']*')|([^\s"'<>\/=`]+))?/; // aaa   =   "value" | 'value' | aaa
-const startTagClose = /^\s*[\/]?>/; // 匹配开始关闭标签     <div   />
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)")|(?:'([^']*)')|([^\s"'<>\/=`]+))?/; // aaa   =   "value" | 'value' | aaa
+const startTagClose = /^\s*(\/?)>/; // 匹配开始关闭标签     <div   />
 const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g; //{{aaaa}}
 // let r = "  />".match(startTagClose);
 // console.log(r);
 
-function start(tagName, attributes) {}
+function start(tagName, attributes) {
+  console.log("start", tagName, attributes);
+}
 
-function end(tagName) {}
+function end(tagName) {
+  console.log("end", tagName);
+}
 
-function chars(text) {}
+function chars(text) {
+  console.log("chars", text);
+}
 
+// 编译字符串得到
 function parseHtml(html) {
-  while (html) {}
+  // 删除字符串
+  function advance(length) {
+    html = html.substring(length);
+  }
+
+  // 解析开始标签并收集属性
+  function parseStartTag() {
+    let start = html.match(startTagOpen);
+    if (start) {
+      // 匹配获取到然后存起来，然后删掉
+      let match = {
+        tagName: start[1],
+        attributes: [],
+      };
+      advance(start[0].length);
+      let end;
+      let attr;
+      // 循环遍历直到匹配到闭合标签结束
+      while (
+        !(end = html.match(startTagClose)) &&
+        (attr = html.match(attribute))
+      ) {
+        // 如果匹配到属性先赋值再将其本身删除
+        // attr = html.match(attribute); // 可以移到条件里
+        match.attributes.push({
+          key: attr[1],
+          value: attr[3] || attr[4] || attr[5],
+        });
+        advance(attr[0].length);
+      }
+      // 解析完开始标签最后把闭合标签给删掉
+      if (end) {
+        advance(end[0].length);
+      }
+      return match;
+    }
+    return false;
+  }
+
+  // 如果模板有内容就一直循环
+  while (html) {
+    // 获取字符串<的位置
+    let startText = html.indexOf("<");
+    if (startText === 0) {
+      // 如果索引为0，有两种情况，1是开始标签 2是闭合开始标签
+      const startTagMatch = parseStartTag();
+      if (startTagMatch) {
+        start(startTagMatch.tagName, startTagMatch.attributes);
+        continue;
+      }
+      const endTagMatch = html.match(endTag);
+      if (endTagMatch) {
+        end(endTagMatch[1]);
+        advance(endTagMatch[0].length);
+        continue;
+      }
+    }
+    //  解析文本
+    let text;
+    // 如果
+    if (startText > 0) {
+      text = html.substring(0, startText);
+    }
+    if (text) {
+      chars(text);
+      advance(text.length);
+    }
+  }
 }
 
 // 编译的类
 export function compilerToFunction(template) {
-  console.log(template);
   parseHtml(template);
 }
